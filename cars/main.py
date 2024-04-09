0f975d163271bf0225036b783357b3b3b6f6df7b
temp <temp@temp.com>
2024-04-08 22:11:58 -0300

    added cars folder to start using k nearest neighbours

---
+++ cars/main.py
@@ -0,0 +1,31 @@
+import sklearn
+from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+from sklearn import linear_model, preprocessing
+
+data = pd.read_csv("car.csv")
+print(data.head())
+
+le = preprocessing.LabelEncoder() 
+
+# we need to use this to normalize the data, so it becomes a number because it doesn't accept str
+
+buying = le.fit_transform(list(data["buying"]))
+
+print("buying: ", buying)
+
+maint = le.fit_transform(list(data["maint"]))
+door = le.fit_transform(list(data["door"]))
+persons = le.fit_transform(list(data["persons"]))
+lug_boot = le.fit_transform(list(data["lug_boot"]))
+safety = le.fit_transform(list(data["safety"]))
+cls = le.fit_transform(list(data["class"]))
+ 
+predict = "class"  #optional
+
+X = list(zip(buying, maint, door, persons, lug_boot, safety))
+y = list(cls)
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
